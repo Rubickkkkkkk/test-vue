43298fc880982a0f97e20075faf61aa6f73d84d8
marvinqi <312508269@qq.com>
2019-04-15 17:38:37 +0800

    feat:test travis

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">Abou</router-link>
     </div>
     <router-view/>
   </div>
